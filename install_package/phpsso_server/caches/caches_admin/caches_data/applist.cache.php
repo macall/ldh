873519163bf26628fb760efdb0e0304572964c6b
HanBo <hanbo@HanBodeMacBook-Pro.local>
2015-12-24 00:36:22 +0800

    init pro

---
+++ install_package/phpsso_server/caches/caches_admin/caches_data/applist.cache.php
@@ -0,0 +1,16 @@
+<?php
+return array (
+  1 => 
+  array (
+    'appid' => '1',
+    'type' => 'phpcms_v9',
+    'name' => 'phpcms v9',
+    'url' => 'http://localhost/phpcms/install_package/',
+    'authkey' => 'xBKOux7rkL6XFe9HhL19LwK9RFnXtm7w',
+    'ip' => '',
+    'apifilename' => 'api.php?op=phpsso',
+    'charset' => 'utf-8',
+    'synlogin' => '1',
+  ),
+);
+?>
